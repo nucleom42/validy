4ff409b32ef06df27f584cc0403aab9e677b1205
olegsaltykov <oleg.saltykov@gmail.com>
2022-09-04 12:37:33 -0400

    ver + specs

--- spec/validy_spec.rb
+++ spec/validy_spec.rb
@@ -2,121 +2,64 @@
 
 require 'spec_helper'
 
-class ValidyFoo
-  include Validy
-  validy_on method: :validate # must be implemented method which includes validations
-
-  attr_accessor :foo, :fool, :foolish
-
-  def initialize(foo = nil, fool = 10, foolish = 0)
-    @foo = foo
-    @fool = fool
-    @foolish = foolish
-  end
-
-  def call
-    return unless valid?
-
-    foo + fool - foolish
-  end
-
-  def validate
-    required(:foo).type(Integer, { type_error: 'not an integer' })
-                  .condition(proc { @foo > 2 }, error: 'foo must be bigger than 2')
-
-    required(:fool).type(Integer).condition(:bigger_than_three?, 'fool must be bigger than 3')
-
-    foolish_is_zero?
-  end
-
-  # user validation method
-  def bigger_than_three?
-    @fool > 3
-  end
-
-  # manual way of setting validation
-  def foolish_is_zero?
-    return unless valid?
-
-    add_error error: 'foolish must be zero' unless @foolish.zero?
-  end
-end
-
-class ValidyFool
-  include Validy
-  validy_on method: :validate!
-
-  attr_accessor :foo, :fool, :foolish
-
-  def initialize(foo = nil, fool = 10, foolish = 0)
-    @foo = foo
-    @fool = fool
-    @foolish = foolish
-  end
-
-  def call
-    return unless valid?
-
-    foo + fool - foolish
-  end
-
-  # must be implemented validate or validate!, the only difference that validate will raise an error in case of invalid
-  def validate!
-    required(:foo).type(Integer, { type_error: 'not an integer' })
-      .condition(proc { @foo > 2 }, error: 'foo must be bigger than 2')
-
-    required(:fool).type(Integer).condition(:bigger_than_three?, 'fool must be bigger than 3')
-
-    foolish_is_zero?
-  end
-
-  # user validation method
-  def bigger_than_three?
-    @fool > 3
-  end
-
-  # manual way of setting validation
-  def foolish_is_zero?
-    return unless valid?
-
-    add_error error: 'foolish must be zero' unless @foolish.zero?
-  end
-end
-
-class FooWithCustomDefinedMethod
-  include Validy
-  validy_on method: :kraken
-
-  attr_accessor :foo
-
-  def initialize(foo = nil)
-    @foo = foo
-  end
-
-  def kraken
-    required(:foo).type(Integer)
-  end
-end
-
-class FooWithCustomDefinedMethodBang
-  include Validy
-  validy_on method: :kraken!
-
-  attr_accessor :foo
-
-  def initialize(foo = nil)
-    @foo = foo
-  end
-
-  def kraken!
-    required(:foo).type(Integer)
-  end
-end
-
 describe Validy do
   describe '#validate' do
+    context 'when no validate action method defined' do
+      let(:klass) do
+        Class.new do
+          include Validy
+          validy_on method: :validate
+        end
+      end
+
+      it 'will raise error' do
+        expect { klass.new }.to raise_error
+      end
+    end
+
+    let(:klass) do
+      Class.new do
+        include Validy
+        validy_on method: :validate # must be implemented method which includes validations
+
+        attr_accessor :foo, :fool, :foolish
+
+        def initialize(foo = nil, fool = 10, foolish = 0)
+          @foo = foo
+          @fool = fool
+          @foolish = foolish
+        end
+
+        def call
+          return unless valid?
+
+          foo + fool - foolish
+        end
+
+        def validate
+          required(:foo).type(Integer, { type_error: 'not an integer' }).condition(proc { @foo > 2 }, error: 'foo must be bigger than 2')
+
+          required(:fool).type(Integer).condition(:bigger_than_three?, 'fool must be bigger than 3')
+
+          foolish_is_zero?
+        end
+
+        # user validation method
+        def bigger_than_three?
+          @fool > 3
+        end
+
+        # manual way of setting validation
+        def foolish_is_zero?
+          return unless valid?
+
+          add_error error: 'foolish must be zero' unless @foolish.zero?
+        end
+      end
+    end
+
     context 'when valid instance' do
-      let(:valid_instance) { ValidyFoo.new(4) }
+      let(:valid_instance) { klass.new(4) }
 
       it 'valid? returns true' do
         expect(valid_instance.valid?).to eq true
@@ -144,15 +87,14 @@ describe Validy do
 
         context 'when validate!' do
           it 'raise an error' do
-            expect { valid_instance.validate! }
-              .to raise_error(Validy::Error).with_message('type_error: not an integer')
+            expect { valid_instance.validate! }.to raise_error(Validy::Error).with_message('type_error: not an integer')
           end
         end
       end
     end
 
     context 'when invalid instance' do
-      let(:invalid_instance) { ValidyFoo.new(1, 11) }
+      let(:invalid_instance) { klass.new(1, 11) }
 
       it 'valid? returns false' do
         expect(invalid_instance.valid?).to eq false
@@ -163,15 +105,56 @@ describe Validy do
       end
 
       it 'validate! raise an error' do
-        expect { invalid_instance.validate! }
-          .to raise_error(Validy::Error).with_message('error: foo must be bigger than 2')
+        expect { invalid_instance.validate! }.to raise_error(Validy::Error).with_message('error: foo must be bigger than 2')
       end
     end
   end
 
   describe '#validate!' do
+    let(:klass_validate_with_bang) do
+      Class.new do
+        include Validy
+        validy_on method: :validate!
+
+        attr_accessor :foo, :fool, :foolish
+
+        def initialize(foo = nil, fool = 10, foolish = 0)
+          @foo = foo
+          @fool = fool
+          @foolish = foolish
+        end
+
+        def call
+          return unless valid?
+
+          foo + fool - foolish
+        end
+
+        # must be implemented validate or validate!, the only difference that validate will raise an error in case of invalid
+        def validate!
+          required(:foo).type(Integer, { type_error: 'not an integer' }).condition(proc { @foo > 2 }, error: 'foo must be bigger than 2')
+
+          required(:fool).type(Integer).condition(:bigger_than_three?, 'fool must be bigger than 3')
+
+          foolish_is_zero?
+        end
+
+        # user validation method
+        def bigger_than_three?
+          @fool > 3
+        end
+
+        # manual way of setting validation
+        def foolish_is_zero?
+          return unless valid?
+
+          add_error error: 'foolish must be zero' unless @foolish.zero?
+        end
+      end
+    end
+
     context 'when valid instance' do
-      let(:valid_instance) { ValidyFool.new(4) }
+      let(:valid_instance) { klass_validate_with_bang.new(4) }
 
       it 'valid? returns true' do
         expect(valid_instance.valid?).to eq true
@@ -189,15 +172,48 @@ describe Validy do
 
     context 'when invalid instance' do
       it 'invalid instance raise error' do
-        expect { ValidyFool.new('1', '11') }.to raise_error(Validy::Error)
-          .with_message('type_error: not an integer')
+        expect { klass_validate_with_bang.new('1', '11') }.to raise_error(Validy::Error).with_message('type_error: not an integer')
       end
     end
   end
 
   context 'when defined custom validation action method' do
+    let(:klass_with_custom_method) do
+      Class.new do
+        include Validy
+        validy_on method: :kraken!
+
+        attr_accessor :foo
+
+        def initialize(foo = nil)
+          @foo = foo
+        end
+
+        def kraken!
+          required(:foo).type(Integer)
+        end
+      end
+    end
+
+    let(:klass_with_custom_method_without_bang) do
+      Class.new do
+        include Validy
+        validy_on method: :kraken
+
+        attr_accessor :foo
+
+        def initialize(foo = nil)
+          @foo = foo
+        end
+
+        def kraken
+          required(:foo).type(Integer)
+        end
+      end
+    end
+
     context 'when wrapped to method without bang' do
-      let(:valid_instance) { FooWithCustomDefinedMethod.new(1) }
+      let(:valid_instance) { klass_with_custom_method_without_bang.new(1) }
 
       it 'valid? returns true' do
         expect(valid_instance.valid?).to eq true
@@ -213,8 +229,8 @@ describe Validy do
       end
     end
 
-    context 'when wrapped to method without bang' do
-      let(:valid_instance) { FooWithCustomDefinedMethodBang.new(1) }
+    context 'when wrapped to method with bang' do
+      let(:valid_instance) { klass_with_custom_method.new(1) }
 
       it 'valid? returns true' do
         expect(valid_instance.valid?).to eq true
@@ -231,8 +247,7 @@ describe Validy do
 
       context 'when instance is invalid' do
         it 'invalid instance raise error' do
-          expect { FooWithCustomDefinedMethodBang.new('1') }
-            .to raise_error(Validy::Error).with_message('error: `1` is not a type Integer')
+          expect { klass_with_custom_method.new('1') }.to raise_error(Validy::Error).with_message('error: `1` is not a type Integer')
         end
       end
     end
--- validy.gemspec
+++ validy.gemspec
@@ -4,9 +4,9 @@ Gem::Specification.new do |s|
   s.author = ['Oleg Saltykov']
   s.homepage = 'https://github.com/nucleom42/validy'
   s.name = 'validy'
-  s.version = '0.1.1'
+  s.version = '1.0.9'
   s.licenses = ['MIT']
-  s.date = '2022-01-09'
+  s.date = '2022-04-09'
   s.summary = 'Validy - simple way to turn ruby instance(object) into validateable object with useful validity helpers'
   s.files = Dir['lib/*']
   s.require_paths = %w[lib]
